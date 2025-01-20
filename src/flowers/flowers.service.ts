07bfb741b2e3c3d1295df2017c8c9ac95c10ae5d
Stanislav Minko <minkostas@gmail.com>
2025-01-20 23:39:11 +0300

    getAllFlowers

--- src/flowers/flowers.service.ts
+++ src/flowers/flowers.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class FlowersService {}
+export class FlowersService {
+    findAll(){
+        return [
+            {
+                name: 'rose',
+                color: 'red',
+                price: 10
+              },
+              {
+                name: 'daisy',
+                color: 'white',
+                price: 5
+              },
+              {
+                name: 'tulip',
+                color: 'yellow',
+                price: 7
+              }
+        ]
+    }
+}

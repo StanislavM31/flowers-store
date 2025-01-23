138040eb534debf7ef2c1672b8eb3d21ad1ce3c9
Stanislav M <minkostas@gmail.com>
2025-01-23 20:35:53 +0300

    pipe example

--- flowers-store/src/flowers/flowers.controller.ts
+++ flowers-store/src/flowers/flowers.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { FlowersService } from './flowers.service';
+import { ParseIntPipe } from 'src/conception/pipe';
 
 @Controller('flowers')
 export class FlowersController {
@@ -8,5 +9,9 @@ export class FlowersController {
   @Get()
   findAll(){
     return this.flowersService.findAll();
+  }
+  checkInt(@Query('pageNumber', ParseIntPipe) pageNumber:number){
+    console.log(pageNumber);
+    return this.flowersService.findAll()
   }
 }
